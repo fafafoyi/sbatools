b79f8e0a9f9a0c929b00d284b35b7bc10ef2878e
fafafoyi <fatihefeunal236@gmail.com>
2025-03-29 21:53:02 +0100

    sbatools4 test

---
+++ test_main.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def test_add():
+
+    assert add(2, 3) == 5
+
+def test_add_negative():
+
+    assert add(-1, 1) == 0
+
+def test_add_zero():
+
+    assert add(0, 0) == 0
